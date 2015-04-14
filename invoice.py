782c3de18756ab2862f9d6abc64d89f5d0bc565a
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2015-04-13 23:02:07 -0300

    Commit afip_cae, afip_cae_due & internal_number with other database cursor.

--- invoice.py
+++ invoice.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import fields, osv
+from openerp.osv import fields, osv, registry
 from openerp.tools.translate import _
 import re
 import logging
@@ -173,13 +173,10 @@ class invoice(osv.osv):
         # TODO: not correct fix but required a frech values before reading it.
         self.write(cr, uid, ids, {})
 
-        conn_obj = self.pool.get('wsafip.connection')
-        serv_obj = self.pool.get('wsafip.server')
-        currency_obj = self.pool.get('res.currency')
-
         Servers = {}
         Requests = {}
         Inv2id = {}
+        Inv2number = {}
 
         for inv in self.browse(cr, uid, ids, context=context):
             journal = inv.journal_id
@@ -218,15 +215,6 @@ class invoice(osv.osv):
                       "Please check the journal sequence prefix and suffix "
                       "are not breaking the number generator."))
 
-            def _f_date(d):
-                return d and d.replace('-', '')
-
-            def _iva_filter(t):
-                return 'IVA' in _get_parents(t.tax_code_id)
-
-            def _not_iva_filter(t):
-                return 'IVA' not in _get_parents(t.tax_code_id)
-
             # Build request dictionary
             if conn.id not in Requests:
                 Requests[conn.id] = {}
@@ -234,63 +222,106 @@ class invoice(osv.osv):
             assert inv.currency_id.afip_code, \
                 'Must defined afip_code for the currency.'
 
-            Requests[conn.id][inv.id] = dict((k, v) for k, v in {
-                'CbteTipo': journal.journal_class_id.afip_code,
-                'PtoVta': journal.point_of_sale,
-                'Concepto': inv.afip_concept,
-                'DocTipo': inv.partner_id.document_type_id.afip_code or '99',
-                'DocNro': int(inv.partner_id.document_number
-                              if inv.partner_id.document_type_id.afip_code
-                              is not None
-                              else False),
-                'CbteDesde': invoice_number,
-                'CbteHasta': invoice_number,
-                'CbteFch': _f_date(inv.date_invoice),
-                'ImpTotal': inv.amount_total,
-                # TODO:
-                # Averiguar como calcular el Importe Neto no Gravado
-                'ImpTotConc': 0,
-                'ImpNeto': inv.amount_untaxed,
-                'ImpOpEx': inv.compute_all(
-                    line_filter=lambda line: len(line.invoice_line_tax_id) == 0
-                )['amount_total'],
-                'ImpIVA': inv.compute_all(
-                    tax_filter=_iva_filter)['amount_tax'],
-                'ImpTrib': inv.compute_all(
-                    tax_filter=_not_iva_filter)['amount_tax'],
-                'FchServDesde': _f_date(inv.afip_service_start)
-                if inv.afip_concept != '1' else None,
-                'FchServHasta': _f_date(inv.afip_service_end)
-                if inv.afip_concept != '1' else None,
-                'FchVtoPago': _f_date(inv.date_due)
-                if inv.afip_concept != '1' else None,
-                'MonId': inv.currency_id.afip_code,
-                'MonCotiz': currency_obj.compute(
-                    cr, uid,
-                    inv.currency_id.id,
-                    inv.company_id.currency_id.id, 1.),
-                'CbtesAsoc': {'CbteAsoc':
-                              [c for c in
-                               self.get_related_invoices(cr, uid, inv.id)]},
-                'Tributos': {'Tributo':
-                             [t for t in self.get_taxes(cr, uid, inv.id)]},
-                'Iva': {'AlicIva':
-                        [a for a in self.get_vat(cr, uid, inv.id)]},
-                'Opcionales': {'Opcional':
-                               [o
-                                for o in self.get_optionals(cr, uid, inv.id)]},
-            }.iteritems() if v is not None)
+            Requests[conn.id][inv.id] = self._new_requst(
+                cr, uid, inv, journal, invoice_number)
             Inv2id[invoice_number] = inv.id
+            Inv2number[invoice_number] = inv.number
+
+        return self._save_cae(cr, uid, Inv2id, Inv2number, Requests)
+
+    def _new_request(self, cr, uid, inv, journal, invoice_number):
+        def _f_date(d):
+            return d and d.replace('-', '')
+
+        def _iva_filter(t):
+            return 'IVA' in _get_parents(t.tax_code_id)
+
+        def _not_iva_filter(t):
+            return 'IVA' not in _get_parents(t.tax_code_id)
+
+        def _remove_nones(d):
+            return {k: v for k, v in d.iteritems() if v is not None}
+
+        currency_obj = self.pool.get('res.currency')
+
+        return _remove_nones({
+            'CbteTipo': journal.journal_class_id.afip_code,
+            'PtoVta': journal.point_of_sale,
+            'Concepto': inv.afip_concept,
+            'DocTipo': inv.partner_id.document_type_id.afip_code or '99',
+            'DocNro': int(inv.partner_id.document_number
+                          if inv.partner_id.document_type_id.afip_code
+                          is not None
+                          else False),
+            'CbteDesde': invoice_number,
+            'CbteHasta': invoice_number,
+            'CbteFch': _f_date(inv.date_invoice),
+            'ImpTotal': inv.amount_total,
+            # TODO:
+            # Averiguar como calcular el Importe Neto no Gravado
+            'ImpTotConc': 0,
+            'ImpNeto': inv.amount_untaxed,
+            'ImpOpEx': inv.compute_all(
+                line_filter=lambda line: len(line.invoice_line_tax_id) == 0
+            )['amount_total'],
+            'ImpIVA': inv.compute_all(
+                tax_filter=_iva_filter)['amount_tax'],
+            'ImpTrib': inv.compute_all(
+                tax_filter=_not_iva_filter)['amount_tax'],
+            'FchServDesde': _f_date(inv.afip_service_start)
+            if inv.afip_concept != '1' else None,
+            'FchServHasta': _f_date(inv.afip_service_end)
+            if inv.afip_concept != '1' else None,
+            'FchVtoPago': _f_date(inv.date_due)
+            if inv.afip_concept != '1' else None,
+            'MonId': inv.currency_id.afip_code,
+            'MonCotiz': currency_obj.compute(
+                cr, uid,
+                inv.currency_id.id,
+                inv.company_id.currency_id.id, 1.),
+            'CbtesAsoc': {'CbteAsoc':
+                          [c for c in
+                           self.get_related_invoices(cr, uid, inv.id)]},
+            'Tributos': {'Tributo':
+                         [t for t in self.get_taxes(cr, uid, inv.id)]},
+            'Iva': {'AlicIva':
+                    [a for a in self.get_vat(cr, uid, inv.id)]},
+            'Opcionales': {'Opcional':
+                           [o for o in self.get_optionals(cr, uid, inv.id)]},
+        })
+
+    def _save_cae(self, cr, uid, Inv2id, Inv2number, Requests):
+        """
+        Store and invoice_number in cae in database using new cursor.
+        @param self: the invoice object.
+        @param cr: database cursor.
+        @param uid: The current user ID for security checks.
+        @param Inv2id: Map of ids from invoice number.
+        @param Inv2number: Map of odoo invoice number from invoice number.
+        """
+        conn_obj = self.pool.get('wsafip.connection')
+        serv_obj = self.pool.get('wsafip.server')
 
         for c_id, req in Requests.iteritems():
             conn = conn_obj.browse(cr, uid, c_id)
             res = serv_obj.wsfe_get_cae(cr, uid, [conn.server_id.id], c_id, req)
             for k, v in res.iteritems():
                 if 'CAE' in v:
-                    self.write(cr, uid, Inv2id[k], {
-                        'afip_cae': v['CAE'],
-                        'afip_cae_due': v['CAEFchVto'],
-                    })
+                    try:
+                        _cr_ = registry(cr.dbname).cursor()
+                        inv_id = Inv2id[k]
+                        inv_number = Inv2number[k]
+                        self.write(_cr_, uid, inv_id, {
+                            'afip_cae': v['CAE'],
+                            'afip_cae_due': v['CAEFchVto'],
+                            'internal_number': inv_number,
+                        })
+                        _cr_.commit()
+                    finally:
+                        try:
+                            _cr_.close()
+                        except Exception:
+                            pass
                 else:
                     # Muestra un mensaje de error por la factura con error.
                     # Se cancelan todas las facturas del batch!
